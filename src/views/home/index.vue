154123b8ff7f2f2354c0d4e2ed3d481bb89d7cc4
haoyueweiliang <1299807407@qq.com>
2019-09-18 18:59:14 +0800

    主页模块 左侧导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>哈哈哈哈</div>
+  <el-container>
+      <el-aside>
+         <layout-aside></layout-aside>
+      </el-aside>
+       <el-container>
+           <el-header>顶部</el-header>
+           <el-main>
+               <!-- 二级容器 -->
+                <router-view></router-view>
+           </el-main>
+  </el-container>
+  </el-container>
+
 </template>
 
 <script>
+import layoutAside from '../../components/aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
