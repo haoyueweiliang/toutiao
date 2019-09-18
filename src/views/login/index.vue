5dad3c3b555d795775edbf922d402ee78c0e481d
haoyueweiliang <1299807407@qq.com>
2019-09-18 17:23:30 +0800

    数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,18 +6,18 @@
         <img src="../../assets/img/logo_index.png" alt="">
     </div>
     <!-- 表单 -->
-<el-form>
-<el-form-item>
-<el-input placeholder="请输入手机号"></el-input>
+<el-form :model='loginForm' ref='myForm' :rules='loginRules'>
+<el-form-item prop='mobile'>
+<el-input v-model='loginForm.mobile' placeholder="请输入手机号"></el-input>
+</el-form-item>
+<el-form-item prop='code'>
+<el-input v-model='loginForm.code' placeholder="请输入验证码" style='width:65%'></el-input><el-button style='float:right'>发送验证码</el-button>
+</el-form-item>
+<el-form-item prop='agree'>
+<el-checkbox v-model='loginForm.agree' >我已阅读并同意用户协议和隐私条款</el-checkbox>
 </el-form-item>
 <el-form-item>
-<el-input placeholder="请输入验证码" style='width:65%'></el-input><el-button style='float:right'>发送验证码</el-button>
-</el-form-item>
-<el-form-item>
-<el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-</el-form-item>
-<el-form-item>
-<el-button type="primary" style='width:100%'>登录</el-button>
+<el-button @click='login' type="primary" style='width:100%'>登录</el-button>
 </el-form-item>
 </el-form>
 </el-card>
@@ -26,7 +26,37 @@
 
 <script>
 export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      value ? callBack() : callBack(new Error('请您勾选同意该协议'))
+    }
+    return {
+      loginForm: { mobile: '', code: '', agree: '' },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请您填写手机号' },
+          { pattern: /^1[3-9]\d{9}$/, message: '请输入正确手机号' }
+        ],
+        code: [
+          { required: true, message: '请您填写验证码' },
+          { pattarn: /^\d{6}$/, message: '请填写正确的验证码' }
+        ],
+        agree: [
+          // 自动效验
+          { validator }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
 
+        }
+      })
+    }
+  }
 }
 </script>
 
