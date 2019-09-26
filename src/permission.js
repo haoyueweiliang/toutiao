e1e7136dbb7d1e31bdeb1b050fd78dfe4f5919b7
haoyueweiliang <1299807407@qq.com>
2019-09-26 22:14:32 +0800

    nprogress进度条  在导航守卫的开启与关闭[C

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,10 @@
 // 导航守卫   看是否有token值  有方行  无则返回首页
 import router from './router'
+// npm i nprogress -S   a安装三方包
+import nprogress from 'nprogress' // 引入进度条
+import 'nprogress/nprogress.css'
 router.beforeEach(function (to, from, next) {
+  nprogress.start() // 开启进度条
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -11,5 +15,10 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+
+// 全局后置守卫
+router.afterEach(function () {
+  nprogress.done() // 关闭进度条
 })
 export default router
